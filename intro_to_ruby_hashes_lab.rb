3f16ce948fbc608021a9172922da471ca04d5cb0
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-01 01:01:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-   base_hash = {:railroads => {:pieces => 4, :rent_in_dollars => {:one => {}, :two => {}, :three => {}, :four => {}}, :names => {:railroads_one => {}, :railroads_two => {}, :railroads_three => {}, :railroads_four => {}}}}
+   base_hash = {:railroads => {:pieces => 4, :rent_in_dollars => {:one_piece_owned => {}, :two => {}, :three => {}, :four => {}}, :names => {:railroads_one => {}, :railroads_two => {}, :railroads_three => {}, :railroads_four => {}}}}
 end
 
 def monopoly_with_fourth_tier
